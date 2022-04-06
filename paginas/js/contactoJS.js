549ca1d40072d175d27dd73a5c672dddf85ddb43
JosVelazquez <ve415477@uaeh.edu.mx>
2022-04-06 17:37:38 -0500

    modificacion

--- paginas/js/contactoJS.js
+++ paginas/js/contactoJS.js
@@ -3,6 +3,82 @@ function precio1 (numero,categ,comple)
     var numero =parseInt(document.getElementById ('cantidad').value);
 var categ =parseFloat(document.getElementById ('categoria').value);
  var comple = parseFloat(document.getElementById('complemento').value);
+ var  entre= parseInt(document.getElementById('entrega').value);
+
+
+ if (isNaN(numero)){
+
+     numero= 0;
+
+ }
+ if (isNaN(categ)){
+
+    categ=0;
+    
+}
+if (isNaN(comple)){
+
+    comple=0;
+    
+}
+if (isNaN(entre)){
+
+    entre=0;
+}
+
+var result = (categ+comple)*numero;
+var res = '$' +result;
+document.getElementById('precio-primero').value =res;
+
+var result = (categ+comple)*numero;
+var res = '$' +result;
+document.getElementById('precio-envio').value =res;
+
+var result = (categ+comple)*numero;
+var res = '$' +result;
+document.getElementById('entrega').value =res;
+
+var result = (categ+comple)*numero ;
+var res = '$' +result;
+document.getElementById('precio-primero').value =res;
+
+}
+
+function preciofinal  (numero,precio1,entrega)
+{
+    var numero =parseInt(document.getElementById ('cantidad').value);
+    var categ =parseFloat(document.getElementById ('categoria').value);
+    var comple = parseFloat(document.getElementById('complemento').value);
+ var entrega  = parseFloat(document.getElementById('entrega').value);
+
+ if (isNaN(numero)){
+
+    numero= 0;
+
+}
+if (isNaN(categ)){
+
+   categ=0;
+   
+}
+if (isNaN(comple)){
+
+   comple=0;
+   
+}
+if (isNaN(entrega)){
+
+   entrega=0;
+   
+}
+ var result = (categ+comple)*numero+entrega;
+var res = '$' +result;
+document.getElementById('precio-envio').value = res;
+
+ var result = (categ+comple)*numero-entrega;
+var res = '$' +result;
+document.getElementById('precio-primero').value =res;
+
 var result = (categ+comple)*numero;
 var res = '$' +result;
 document.getElementById('precio-primero').value =res;
@@ -12,24 +88,13 @@ document.getElementById('precio-primero').value =res;
 
 
 
-function preciofinal  (numero,precio1,entrega)
-{
-    var numero =parseInt(document.getElementById ('cantidad').value);
-    var categ =parseFloat(document.getElementById ('categoria').value);
-    var comple = parseFloat(document.getElementById('complemento').value);
- var entrega  = parseFloat(document.getElementById('entrega').value);
-
- var result = (categ+comple)*numero+entrega;
-var res = '$' +result;
-document.getElementById('precio-envio').value =res;
-
-}
-
 function sumar (valor) {
     var numero = parseInt(document.getElementById('cantidad').value);
     var categ =parseFloat(document.getElementById ('categoria').value);
  var comple = parseFloat(document.getElementById('complemento').value);
-	var result = (categ+comple)*numero;
+    var result = (categ+comple)*numero;
+    var result = (categ-comple)*numero;
+
 
     
 }
@@ -37,26 +102,33 @@ function sumar (valor) {
 
 function mostrar_pago(){
     document.getElementById('efectivo').style.display ='block';
+    document.getElementById('transferencia').style.display ='none';
+      document.getElementById('tarjeta').style.display ='none';
+       document.getElementById('paypal').style.display ='none';
     
     
  
 }
 function mostrar_pago2(){
-
+    document.getElementById('efectivo').style.display ='none';
     document.getElementById('transferencia').style.display ='block';
-    
+    document.getElementById('tarjeta').style.display ='none';
+       document.getElementById('paypal').style.display ='none';
  
 }
 function mostrar_pago3(){
-    
+     document.getElementById('efectivo').style.display ='none';
+     document.getElementById('transferencia').style.display ='none';
     document.getElementById('tarjeta').style.display ='block';
-   
+    document.getElementById('paypal').style.display ='none';
     
  
 }
 
 function mostrar_pago4(){
-    
+    document.getElementById('efectivo').style.display ='none';
+    document.getElementById('transferencia').style.display ='none';
+    document.getElementById('tarjeta').style.display ='none';
     document.getElementById('paypal').style.display ='block';
    
     
@@ -78,17 +150,28 @@ $("#compra").on("submit",function(event){
 });
 
 
-
+function entrega(){
+    var entrega = parseFloat(document.getElementById('entrega'.value));
+    document.getElementById('entrega').value ;
+}
 
 function mensaje (){
     
     var respuesta= confirm ("Tu pedido ha sido confirmado gracias por tu compra  "  ) ;
 
     if(respuesta==true){
-      return true  ;
+      return formulario  ;
+      
     
     }
     else{
       return false ;
     }
+  }
+
+
+ 
+  function limpiar () {
+    var respuesta= confirm ("El formulario sera limpiado  "  ) ;
+document.getElementById("boton-enviar2").reset();
   }
